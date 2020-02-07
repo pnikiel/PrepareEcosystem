393423ecff326b4f4b5e8091c6817dee74963564
Piotr Nikiel <piotr@nikiel.info>
2020-02-07 10:20:01 +0100

    aligned to new changes in LogIt

--- prepare.sh
+++ prepare.sh
@@ -8,13 +8,14 @@ prep_LogIt()
 {
 cd $WORK
 LOGIT_URL="https://github.com/quasar-team/LogIt.git"
-LOGIT_TAG="OPCUA-1671-compile-as-stand-alone-static-lib-with-LCG-96"
+LOGIT_TAG="OPCUA-1710-install-target-for-LogIt"
 rm -fr LogIt
 git clone $LOGIT_URL --depth=1 -b $LOGIT_TAG LogIt || exit
 cd LogIt
 mkdir build && cd build
-cmake -DLOGIT_BUILD_STAND_ALONE=ON -DLOGIT_BUILD_STATIC_LIB=ON -DCMAKE_INSTALL_PREFIX=$DEPLOYMENT  ../
+cmake -DLOGIT_BUILD_STAND_ALONE=ON -DLOGIT_BUILD_STATIC_LIB=ON -DCMAKE_INSTALL_PREFIX=$DEPLOYMENT/LogIt  ../
 make || exit
+make install || exit
 # there we should have make install, but doesn't yet exist in open62541-compat
 cd $WD
 }
@@ -28,7 +29,7 @@ rm -fr open62541-compat
 git clone $OPEN62541_COMPAT_URL --depth=1 -b $OPEN62541_COMPAT_TAG || exit
 cd open62541-compat
 mkdir build && cd build
-cmake -DSTANDALONE_BUILD=ON -DLOGIT_BUILD_OPTION=LOGIT_AS_EXT_STATIC -DOPEN62541-COMPAT_BUILD_CONFIG_FILE=boost_lcg.cmake -DLOGIT_INCLUDE_DIR=$WORK/LogIt/include -DLOGIT_EXT_LIB_DIR=$WORK/LogIt/build -DCMAKE_INSTALL_PREFIX=$DEPLOYMENT/open62541-compat -DSKIP_TESTS=ON  ../ || exit
+cmake -DSTANDALONE_BUILD=ON -DLOGIT_BUILD_OPTION=LOGIT_AS_EXT_STATIC -DOPEN62541-COMPAT_BUILD_CONFIG_FILE=boost_lcg.cmake -DLOGIT_INCLUDE_DIR=$DEPLOYMENT/LogIt/LogIt/include -DLOGIT_EXT_LIB_DIR=$DEPLOYMENT/LogIt/lib -DCMAKE_INSTALL_PREFIX=$DEPLOYMENT/open62541-compat -DSKIP_TESTS=ON  ../ || exit
 make || exit
 make install || exit
 cd $WD
